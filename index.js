d4c65475339e82fe355683f250dbd61908ce6162
Carlos Eduardo Lourenço De Souza <97536308+carloseduardolsouza@users.noreply.github.com>
2024-02-22 11:23:13 -0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow} = require("electron")
 
 function createWindow() {
-    const win = BrowserWindow({width: 800 , height: 600})
+    const win = new BrowserWindow({width: 800 , height: 600})
     win.loadURL("http://localhost:3000/")
 }
 
